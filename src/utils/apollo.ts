1647577389bb4f0facccebc990fb237be3913c43
linuxing3 <linuxing3@qq.com>
2019-06-11 18:04:29 -0400

    test: integrate apollo

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,9 +1,7 @@
-import Vue from 'vue';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import VueApollo from 'vue-apollo';
-Vue.use(VueApollo);
 
 const httpLink = new HttpLink({
   // 你需要在这里使用绝对路径
--- src/utils/use.ts
+++ src/utils/use.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
-// import ViserVue from 'viser-vue';
 import VueRx from 'vue-rx';
-import './apollo';
+import VueApollo from 'vue-apollo'
 
+Vue.use(VueApollo)
 Vue.use(VueI18n);
-// Vue.use(ViserVue);
 Vue.use(VueRx);
+
+// import ViserVue from 'viser-vue';
+// Vue.use(ViserVue);
